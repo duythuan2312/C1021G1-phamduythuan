6fca961c396bf5e019688582946a48d03c9885ce
thuan <you@example.com>
2022-04-07 10:24:52 +0700

    phamduythuan_c1021g1

---
+++ module5/5. Components-Interaction/exercise/angular-rating-bar/src/app/IRating.ts
@@ -0,0 +1,5 @@
+export interface IRating {
+  value: string;
+  active: boolean;
+
+}
---
+++ module5/5. Components-Interaction/exercise/angular-rating-bar/src/app/rating-bar/rating-bar.component.ts
@@ -0,0 +1,58 @@
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {IRating} from '../IRating';
+
+@Component({
+  selector: 'app-rating-bar',
+  templateUrl: './rating-bar.component.html',
+  styleUrls: ['./rating-bar.component.css']
+})
+export class RatingBarComponent implements OnInit,OnChanges  {
+
+  @Input()
+  max = 100;
+  @Input()
+  ratingValue = 1;
+  @Input()
+  showRatingValue = true;
+
+  @Output()
+  rateChange = new EventEmitter<number>();
+
+  ratingUnits: Array<IRating> = [];
+
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ('max' in changes) {
+      let max = changes.max.currentValue;
+      max = typeof max === 'undefined' ? 5 : max;
+      this.max = max;
+      this.calculate(max, this.ratingValue);
+    }
+  }
+
+  calculate(max, ratingValue) {
+    this.ratingUnits = Array.from({length: max},
+      (_, index) => ({
+        value: index + 1,
+        active: index < ratingValue
+      }));
+  }
+
+  ngOnInit() {
+    this.calculate(this.max, this.ratingValue);
+  }
+
+  select(index) {
+    this.ratingValue = index + 1;
+    this.ratingUnits.forEach((item, idx) => item.active = idx < this.ratingValue);
+    this.rateChange.emit(this.ratingValue);
+  }
+  enter(index) {
+    this.ratingUnits.forEach((item, idx) => item.actice = idx <= index);
+  }
+  reset() {
+    this.ratingUnits.forEach((item, idx) => item.active = idx < this.ratingValue);
+  }
+
+}
